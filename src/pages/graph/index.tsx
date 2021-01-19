4f3a76af97aebcc7844018605a2e8600debbeea8
otaviobonder <otaviobonder@gmail.com>
2021-01-19 16:06:22 -0300

    commented functions

--- src/pages/graph/index.tsx
+++ src/pages/graph/index.tsx
@@ -3,7 +3,13 @@ import lightningGraph from "../../assets/lightning_graph.json";
 import { IGraph } from "./interfaces";
 import { Graph } from "../../components/Graph";
 
-// Importing directly from file. Usually this would be an API call
+/*
+Importing directly from file. Usually this would be an API call.
+Also, I decided to remove some unnecessary info from the graph file,
+since I only need some attributes. I chose to save the result of the
+map function in an useMemo hook, so the function is only executed once.
+*/
+
 const leanLightningGraph = () => {
   const nodes = (lightningGraph as IGraph).nodes.map((node) => ({
     publicKey: node.pub_key,
