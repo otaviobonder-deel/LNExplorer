1f83aa08d1484845679d597a4b70f83be5c6698b
otaviobonder <otaviobonder@gmail.com>
2021-01-20 10:40:59 -0300

    fixed graph props

--- src/components/Graph/index.tsx
+++ src/components/Graph/index.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import ForceGraph from "react-force-graph-3d";
 import { IProps } from "./interfaces";
 
-export const Graph: React.FC<IProps> = ({ graph }) => (
+export const Graph: React.FC<IProps> = ({ graph, onNodeClick }) => (
   <ForceGraph
     graphData={graph}
     nodeId="publicKey"
+    onNodeClick={onNodeClick}
     linkSource="node1"
     linkTarget="node2"
+    nodeResolution={8}
+    cooldownTime={3000}
+    rendererConfig={{
+      powerPreference: "high-performance",
+    }}
+    enableNodeDrag={false}
   />
 );
