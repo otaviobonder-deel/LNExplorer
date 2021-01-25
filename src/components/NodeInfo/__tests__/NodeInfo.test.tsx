d026fbf97773d140fe0775ee65f71356987eb5ea
otaviobonder <otaviobonder@gmail.com>
2021-01-25 11:59:28 -0300

    added test to check if component is not crashing

--- src/components/NodeInfo/__tests__/NodeInfo.test.tsx
+++ src/components/NodeInfo/__tests__/NodeInfo.test.tsx
@@ -8,7 +8,7 @@ response from the api. But since I'm getting the info from
 a static JSON file, there's no sense in testing it
 */
 
-it("should render a test node info", () => {
+it("should match the snapshot", () => {
   const info = {
     publicKey: "test123",
     alias: "test",
@@ -29,4 +29,24 @@ it("should render a test node info", () => {
     <NodeInfo graph2ScreenCoords={graph2ScreenCoords} info={info} />
   );
   expect(component.container).toMatchSnapshot();
+});
+
+it("should render without crashing", () => {
+  const info = {
+    publicKey: "test123",
+    alias: "test",
+    color: "#fff",
+    visible: true,
+    links: [
+      {
+        channelId: "123",
+        node1: "test123",
+        node2: "test345",
+        capacity: "10000",
+        color: "#fff",
+      },
+    ],
+  };
+  const graph2ScreenCoords = () => ({ x: 0, y: 0, z: 0 });
+  render(<NodeInfo graph2ScreenCoords={graph2ScreenCoords} info={info} />);
 });
